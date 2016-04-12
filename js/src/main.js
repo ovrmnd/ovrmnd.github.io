6fd28e0d90f60a10cbfb6b6fe66d9e4b50e26f98
Kyle Brumm <kjbrumm@gmail.com>
2016-04-12 11:02:06 -0500

    Submit contact form with ajax and add services

--- js/src/main.js
+++ js/src/main.js
@@ -1,20 +1,45 @@
 (function($) {
-    $('input').keyup(function(e){
-        if($(this).val()) {
-            $(this).addClass('filled');
-        } else {
-            $(this).removeClass('filled');
-        }
-    });
 
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
+    /**
+     *  Toggle whether a form fields is filled
+     */
+    var toggleFilled = function(el) {
+        if($(el)) {
+            $(el).addClass('filled');
+        } else {
+            $(el).removeClass('filled');
+        }
+    };
+
+    /**
+     *  Add 'filled' class to form elements if they have a value
+     */
+    $('input, textarea').keyup(function(e){
+        toggleFilled(this);
+    });
     $('select' ).change(function(e) {
-        if($(this).val()) {
-            $(this).addClass('filled');
-        } else {
-            $(this).removeClass('filled');
-        }
+        toggleFilled(this);
     });
 
+    /**
+     *  Toggle the mobile menu
+     */
     $('.mobile-menu').click(function(e) {
         e.preventDefault();
         $('.nav').toggleClass('open');
@@ -23,13 +48,29 @@
 
 
     /**
-     *  Scroll window to an element
+     *  Smooth scroll window to an element
      */
     $('a[href^="#"').click(function(e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: ($($.attr(this, 'href')).offset().top) - $('.header').height()
         }, 500);
+    });
+
+    $('.form-contact').submit(function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "https://formspree.io/kjbrumm@gmail.com",
+            method: "POST",
+            data: $(this).serializeObject(),
+            dataType: "json"
+        }).done(function(data) {
+            if(data.success) {
+                $('.form-contact').hide();
+                $('.form-notice').text('Your message has been sent. We will be in touch shortly!').fadeIn();
+            }
+        });
     });
 
     // $(window).scroll(function(e) {
